dbbf366e5b3e769c348367d690564fece887e6b4
Vatsal Salla <sallavatsal@gmail.com>
2019-03-23 10:15:29 -0500

    Update binarySearchRecursive.c

--- binarySearchRecursive.c
+++ binarySearchRecursive.c
@@ -29,3 +29,36 @@ int main(){
     
     return 0;
 }
+
+/* Time Complexity of Binary Search Algorithm
+    
+    Step 1: Applying binary search algorithm to an array of length n
+            Next will be n/2. Next will be n/4 and So on... 
+            
+    Step 2: Time complexity of whole array T(n)
+            
+            T(n) = T(n/2) + c       // c is a constant as the array keeps on repeating itself
+    
+    Recursive Tree:
+            T(n)     ->     c +     ->     c +   ....   ->  c +
+                          T(n/2)           c +              c +
+                                          T(n/4)            .
+                                                            .
+                                                            .
+                                                            .
+                                                            T(1)
+                                                            
+                                                            Number of Cs (constants) = k
+                                                            
+            Left to right: To go from T(n)  ->  Divide by 2    ->  T(n/2) -> ....... T(1)
+            Right to left: To go from T(1)  ->  Multiply by 2  ->  ....... T(n/2) -> T(n)
+            
+            Thus, using right to left approach:
+            
+                    1*(2^k) = n     // Apply Log on both sides
+            Thus,   k = log(n)      // Base 2
+            
+            So, k * (c) + T(1) ~ log(n)
+            
+            Time Complexity: O(log(n))
+*/
