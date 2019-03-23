9c0c493080668d643e157081ec11f20f16718dc1
Vatsal Salla <sallavatsal@gmail.com>
2019-03-23 10:17:07 -0500

    Update binarySearchRecursive.c

--- binarySearchRecursive.c
+++ binarySearchRecursive.c
@@ -10,8 +10,8 @@ bool binarySearchRecursive(int *arr, int x, int L, int R){                  // C
 
     mid = ((L + R)/2);                                                      // Calculating the middle term
     
-    if(arr[mid]>x) return binarySearchRecursive(arr, x, L, mid-1);          // Middle term: arr[mid] on right
-    else if(arr[mid]<x) return binarySearchRecursive(arr, x, mid+1, R);     // Middle term: arr[mid] on right
+    if(arr[mid]>x) return binarySearchRecursive(arr, x, L, mid-1);          // Middle term: arr[mid] on right - Delete Upper
+    else if(arr[mid]<x) return binarySearchRecursive(arr, x, mid+1, R);     // Middle term: arr[mid] on left - Delete Lower
     
     else return true;
 }
